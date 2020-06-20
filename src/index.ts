7bf398a2ff78b83e81e0c0a28f68d952e7efd78b
Eugene <hronom@gmail.com>
2020-06-20 19:48:51 +0300

    Switch to WebStorm usage

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,22 @@ import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
 
 app.on('ready', () => {
-  console.log('App is ready');
+    console.log('App is ready');
 
-  const win = new BrowserWindow({
-    width: 600,
-    height: 400,
-    title: "electron-typescript-playground"
-  });
+    const win = new BrowserWindow({
+        width: 600,
+        height: 400,
+        title: "electron-typescript-playground"
+    });
 
-  app.setBadgeCount(3);
+    app.setBadgeCount(3);
 
-  const indexHTML = path.join(__dirname + '/index.html');
-  win.loadFile(indexHTML).then(() => {
-    // IMPLEMENT FANCY STUFF HERE
-  }).catch(e => console.error(e));
+    const indexHTML = path.join(__dirname + '/index.html');
+    win.loadFile(indexHTML).then(() => {
+        // IMPLEMENT FANCY STUFF HERE
+    }).catch(e => console.error(e));
 });
+
+app.on("will-quit", () => {
+    console.log('App is closing');
+})
